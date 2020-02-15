1590e78d510edfeee56cbfda077b18183e9ca8af
TheIllusiveC4 <itwasmec4@gmail.com>
2020-02-14 18:09:57 -0800

    update license

--- src/main/java/top/theillusivec4/beaconsforall/util/ReflectionAccessor.java
+++ src/main/java/top/theillusivec4/beaconsforall/util/ReflectionAccessor.java
@@ -1,20 +1,20 @@
 /*
- * Copyright (C) 2018-2019  C4
+ * Copyright (c) 2018-2020 C4
  *
- * This file is part of Creatures Love Beacons, a mod made for Minecraft.
+ * This file is part of Beacons For All, a mod made for Minecraft.
  *
- * Creatures Love Beacons is free software: you can redistribute it and/or modify it
+ * Beacons For All is free software: you can redistribute it and/or modify it
  * under the terms of the GNU Lesser General Public License as published
  * by the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
- * Creatures Love Beacons is distributed in the hope that it will be useful, but
+ * Beacons For All is distributed in the hope that it will be useful, but
  * WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR PARTICULAR PURPOSE.  See the
  * GNU Lesser General Public License for more details.
  *
  * You should have received a copy of the GNU Lesser General Public
- * License along with Creatures Love Beacons.  If not, see <https://www.gnu.org/licenses/>.
+ * License along with Beacons For All.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package top.theillusivec4.beaconsforall.util;
